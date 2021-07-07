95532d1aec49d28da3f6b2756fc803d60d06d997
prav77 <prablemo@outlook.com>
2021-07-07 12:30:12 +0545

    web scrapping using selenium

---
+++ webscraping_example/webscraping_example.py
@@ -0,0 +1,23 @@
+import time
+from selenium import webdriver 
+PATH = "C:/ProgramData/Anaconda3/scripts/chromedriver.exe" #always keeps chromedriver.exe inside scripts to save hours of debugging
+driver =webdriver.Chrome(PATH) #preety i,portant part
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+languages = driver.find_elements_by_class_name('interlanguage-link')
+language_names = [language.find_element_by_css_selector('a').text 
+                 for language in languages]
+
+links = [lang.find_element_by_css_selector('a').get_attribute('href') 
+        for lang in languages]
+content_element = driver.find_element_by_id('n-contents')
+
+content_element.click()
+urls = links[0:5] # changes link starting from 0 to 5 link
+
+for url in urls:
+    driver.get(url)
+    # stop for 2 seconds before going for the next page
+    time.sleep(2)
+print(links)
+print(language_names)
+driver.close()
