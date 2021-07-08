a3da7ed52e229324f85d36539831fa5308ad3e23
prav77 <prablemo@outlook.com>
2021-07-09 01:31:11 +0545

    imported apis

--- webscraping_example/webscraping_example.py
+++ webscraping_example/webscraping_example.py
@@ -1,23 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
 import time
+import pandas as pd
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import csv
 from selenium import webdriver 
 PATH = "C:/ProgramData/Anaconda3/scripts/chromedriver.exe" #always keeps chromedriver.exe inside scripts to save hours of debugging
 driver =webdriver.Chrome(PATH) #preety i,portant part
-driver.get("")
-languages = driver.find_elements_by_class_name('interlanguage-link') #do it yes 
-language_names = [language.find_element_by_css_selector('a').text 
-                 for language in languages]
+driver.get("https://hamrobazaar.com/")
+#languages = driver.find_elements_by_class_name('interlanguage-link') #do it yes 
 
-links = [lang.find_element_by_css_selector('a').get_attribute('href') 
-        for lang in languages]
-content_element = driver.find_element_by_id('n-contents')
-
-content_element.click()
-urls = links[0:5] # changes link starting from 0 to 5 link
-
-for url in urls:
-    driver.get(url)
-    # stop for 2 seconds before going for the next page
-    time.sleep(2)
-print(links)
-print(language_names)
-driver.close()
