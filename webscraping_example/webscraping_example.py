89ae9930ebb70988694dde5f4c4285eb136419e1
prav77 <prablemo@outlook.com>
2021-07-10 21:01:20 +0545

    a working code with csv file

--- webscraping_example/webscraping_example.py
+++ webscraping_example/webscraping_example.py
@@ -14,11 +14,16 @@ PATH = "C:/ProgramData/Anaconda3/scripts/chromedriver.exe" #always keeps chromed
 driver =webdriver.Chrome(PATH) #preety important part
 driver.get("https://www.gharghaderi.com/")
 driver.implicitly_wait(10)
-# house = driver.find_elements_by_class_name('griddetails')
-# for x in house:
-#     driver.get(x)
-# print(x.text)
 house = driver.find_elements_by_class_name('griddetails')
 for x in house:
    print(x.text)
+   house_list = []
+house = driver.find_elements_by_class_name('griddetails')
+for x in house:
+   house_list.append(x.text)
+data = {
+         'Details': house_list
+        }
+df = pd.DataFrame.from_dict(data)
+df.to_csv('out.csv', index = 0)
 
