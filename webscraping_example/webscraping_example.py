9710eea991a4f6175c64fd94a9df238a205a9546
prav77 <prablemo@outlook.com>
2021-07-23 21:15:17 +0545

    this is it

--- webscraping_example/webscraping_example.py
+++ webscraping_example/webscraping_example.py
@@ -12,14 +12,48 @@ import csv
 from selenium import webdriver 
 PATH = "C:/ProgramData/Anaconda3/scripts/chromedriver.exe" #always keeps chromedriver.exe inside scripts to save hours of debugging
 driver =webdriver.Chrome(PATH) #preety important part
+driver.implicitly_wait(20)
 driver.get("https://www.gharbazar.com/property/search/?_q=&_vt=1&_r=0&_pt=residential&_si=0&_srt=latest")
-driver.implicitly_wait(10)
-# house=driver.find_elements_by_tag_name("a")
-# for lnk in house:
-#    print(lnk.get_attribute('href'))
-house = driver.find_elements_by_xpath("//div[@id = 'search-properties']/a")
-for links in house:
-   print(links.get_attribute('href'))
+links = driver.find_elements_by_xpath("//div[@id = 'search-properties']/a")
+for ele in links:
+   print(ele.get_attribute('href'))
+page= 2
+pagelinks= []
+    #links of the 1st page
+links = driver.find_elements_by_xpath("//div[@id = 'search-properties']/a")
+for ele in links:
+        pagelinks.append(ele.get_attribute('href'))
+
+while True:
+        nextoption = driver.find_element_by_xpath("//div[@id='pagination-div']//a[contains(text(),'>>')]")
+        driver.execute_script("arguments[0].scrollIntoView(true);",nextoption)
+        driver.execute_script("window.scrollBy(0,-300)")
+        time.sleep(5)
+        try:
+            driver.find_element_by_link_text(str(page)).click()
+            page += 1
+            links = driver.find_elements_by_xpath("//div[@id = 'search-properties']/a")
+            for ele in links:
+                pagelinks.append(ele.get_attribute('href'))
+            time.sleep(3)
+
+        except Exception as e:
+            print(e)
+            break
+print(len(pagelinks))
+for i in range(len(pagelinks)):
+        print(pagelinks[i])
+data = {
+         'Links':pagelinks 
+        }
+df = pd.DataFrame.from_dict(data)
+df.to_csv('housinglinks.csv', index = 0)        
+
+
+
+  
+        
+
 
 
 
@@ -31,16 +65,3 @@ for links in house:
 
 
    
-#    house_list = []
-# for x in house:
-#    house_list.append(x.text)
-# data = {
-#          'Details': house_list,
-#          'roads' : house_list
-#         }
-# df = pd.DataFrame.from_dict(data)
-# df.to_csv('housingresult.csv', index = 0)
-#this comment should not be visible in another branch....
-
-
-
