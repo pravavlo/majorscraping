cd56fc891b7b6c9d54a962ccff544dfb327edd87
prav77 <prablemo@outlook.com>
2021-07-26 16:33:50 +0545

    added stuff removed stuffs

--- webscraping_example/gharbazar_links.py
+++ webscraping_example/gharbazar_links.py
@@ -12,21 +12,6 @@ import csv
 from selenium import webdriver 
 PATH = "C:/ProgramData/Anaconda3/scripts/chromedriver.exe" #always keeps chromedriver.exe inside scripts to save hours of debugging
 driver =webdriver.Chrome(PATH) #preety important part
-driver.get("https://www.gharghaderi.com/")
-driver.implicitly_wait(10)
-house = driver.find_elements_by_class_name('griddetails')
-for x in house:
-   print(x.text)
-   house_list = []
-house = driver.find_elements_by_class_name('griddetails')
-for x in house:
-   house_list.append(x.text)
-data = {
-         'Details': house_list
-        }
-df = pd.DataFrame.from_dict(data)
-df.to_csv('housingprices.csv')
-driver.implicitly_wait(20)
 driver.get("https://www.gharbazar.com/property/search/?_q=&_vt=1&_r=0&_pt=residential&_si=0&_srt=latest")
 links = driver.find_elements_by_xpath("//div[@id = 'search-properties']/a")
 for ele in links:
@@ -62,4 +47,4 @@ data = {
         }
 df = pd.DataFrame.from_dict(data)
 df.to_csv('housinglinks.csv', index = 0)  
-#let this not be seen 
+driver.quit()
