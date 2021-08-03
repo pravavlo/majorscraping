33a13d646d00b40e3728fd7bf5f80eb84ff6eee6
prav77 <prablemo@outlook.com>
2021-08-03 21:17:44 +0545

    new links of nepal homes

--- webscraping_example/nepal_homes.py
+++ webscraping_example/nepal_homes.py
@@ -9,32 +9,27 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import csv
+import re 
 from selenium import webdriver 
 PATH = "C:/ProgramData/Anaconda3/scripts/chromedriver.exe" #always keeps chromedriver.exe inside scripts to save hours of debugging
 driver =webdriver.Chrome(PATH) #preety important part
-driver.get("https://www.gharghaderi.com/house-for-sale/")
-driver.implicitly_wait(10)
+driver.get("https://www.nepalhomes.com/list/&sort=1&find_property_purpose=5db2bdb42485621618ecdae6&find_property_category=5d660cb27682d03f547a6c4a")
+driver.implicitly_wait(20)
+time.sleep(5)
+home_links=[]  
 def isDisplayed():
     try:
-        driver.find_element_by_xpath('//span[@class="show_more"]/button')
+        driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/div/div[2]/div[3]/button')
     except NoSuchElementException:
         return False
     return True
 while(isDisplayed()):
     try:
-        show_more_listings = driver.find_element_by_xpath('//span[@class="show_more"]/button')
+        nepahome_links=driver.find_elements_by_xpath("//div[@id='app']/div[1]/div/div/div/div[2]/div[3]/a")
+        for ele in nepahome_links:
+         print(ele.get_attribute('href'))
+        show_more_listings = driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/div/div[2]/div[3]/button')
         show_more_listings.click()
-        time.sleep(3)
+        time.sleep(3)       
     except:
         print("you have reached end of the list no more houses to be shown")    
-# for x in house:
-#    print(x.text)
-#    house_list = []
-# house = driver.find_elements_by_class_name('griddetails')
-# for x in house:
-#    house_list.append(x.text)
-# data = {
-#          'Details': house_list
-#         }
-# df = pd.DataFrame.from_dict(data)
-# df.to_csv('housingprices.csv')
