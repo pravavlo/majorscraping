2e0e5982a2196e6b15620930d1c359804128f25f
prav77 <prablemo@outlook.com>
2021-08-14 20:52:44 +0545

    brought back the header cause realised without list cannot be converted and converted lisnks into data

--- webscraping_example/nepal_homes.py
+++ webscraping_example/nepal_homes.py
@@ -9,12 +9,15 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import csv
-import re 
+import re
 from selenium import webdriver 
 PATH = "C:/ProgramData/Anaconda3/scripts/chromedriver.exe" #always keeps chromedriver.exe inside scripts to save hours of debugging
 driver =webdriver.Chrome(PATH) #preety important part
 driver.get("https://www.nepalhomes.com/list/&sort=1&find_property_purpose=5db2bdb42485621618ecdae6&find_property_category=5d660cb27682d03f547a6c4a")
 driver.implicitly_wait(10)
+data_extract= pd.read_csv(r'F:\github projects\homie.csv')
+de = data_extract['Links'].tolist()
+print('array:',de)
 
 
 
@@ -33,20 +36,22 @@ driver.implicitly_wait(10)
 
 
 
-home_links=[]
-try:
-    nepahome_links=driver.find_elements_by_xpath("//div[@id='app']/div[1]/div/div/div/div[2]/div[3]/a")
-    for ele in nepahome_links:
-        home_links.append(ele.get_attribute('href'))   
-except NoSuchElementException:
-    print("end")
 
-print(len(home_links))
-for i in range(len(home_links)):
-    print(home_links[i]) 
-data = {
-    'Links':home_links 
-    }
-df = pd.DataFrame.from_dict(data)
-df.to_csv('homie.csv', index = 0,  header=False)  
-driver.quit()
+
+# home_links=[]
+# try:
+#     nepahome_links=driver.find_elements_by_xpath("//div[@id='app']/div[1]/div/div/div/div[2]/div[3]/a")
+#     for ele in nepahome_links:
+#         home_links.append(ele.get_attribute('href'))   
+# except NoSuchElementException:
+#     print("end")
+
+# print(len(home_links))
+# for i in range(len(home_links)):
+#     print(home_links[i]) 
+# data = {
+#     'Links':home_links 
+#     }
+# df = pd.DataFrame.from_dict(data)
+# df.to_csv('homie.csv', index = 0, header=True)  
+# driver.quit()
